735239622628477c42b84d6acf9a91711223b7d2
Rohit Jadhav <rjdhav67@gmail.com>
2026-04-25 01:04:35 +0530

    final posthog

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,11 +1,14 @@
 import posthog from "posthog-js";
 
-const posthogToken = process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN;
-const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+const posthogToken =
+  process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN ||
+  "phc_wnNK7aCrkKPq3PiWGeGL7nfLkQBt4VU4cnNYfYsghte9";
+const posthogHost =
+  process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com";
 
 if (posthogToken) {
   posthog.init(posthogToken, {
-    api_host: posthogHost || "https://us.i.posthog.com",
+    api_host: posthogHost,
     defaults: "2026-01-30",
   });
 }
